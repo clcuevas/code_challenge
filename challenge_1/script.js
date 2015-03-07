a3edd503ef7ca2f9d71c17d2c552298140963702
Claudia Cuevas <omordn@gmail.com>
2015-03-07 15:26:34 -0800

    working JSON status

--- challenge_1/script.js
+++ challenge_1/script.js
@@ -1,8 +1,19 @@
 $(function() {
-	var url = "../data.json"; //JSON data
+	/*need to find a fix for chrome to see local JSON file*/
+	var url = "data.json"; //JSON data, DOES NOT WORK WITH CHROME
 
 	$.getJSON(url, function(response) {
 		var employeesHTML = '<ul class="bulleted">';
+		$.each(response, function(index, employee) {
+			if (employee.occupation === 'Programmer') {
+				employeesHTML += '<li class="in">';
+			} else {
+				employeesHTML += '<li class="out">';
+			}
+			employeesHTML += employee.name + '</li>';
+		});
+		employeesHTML += '</ul>';
+		$('#employeeList').html(employeesHTML);
 	}); //end getJSON
 
 }); //end ready
