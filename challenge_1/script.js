e4faff99e746a9c3b98ca35b165af36889c90125
Claudia Cuevas <omordn@gmail.com>
2015-03-07 14:20:07 -0800

    updating HTML and CSS challenge 1 files

--- challenge_1/script.js
+++ challenge_1/script.js
@@ -1,1 +1,8 @@
-var employees = JSON.parse();
+$(function() {
+	var url = "../data.json"; //JSON data
+
+	$.getJSON(url, function(response) {
+		var employeesHTML = '<ul class="bulleted">';
+	}); //end getJSON
+
+}); //end ready
