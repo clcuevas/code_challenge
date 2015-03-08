0f58e34f47fde1bb2a6e050d543dd942ee13a36e
Claudia Cuevas <omordn@gmail.com>
2015-03-07 21:06:33 -0800

    minor changes

--- challenge_1/script.js
+++ challenge_1/script.js
@@ -4,6 +4,8 @@ $(function() {
 
 	$.getJSON(url, function(response) {
 		var employeesHTML = '<ul class="bulleted">';
+		var avgIndex = '<h2>Avg Awesome Index: ';
+		var avg;
 
 		$.each(response, function(index, employee) {
 			if (employee.occupation === 'Programmer') {
@@ -12,13 +14,27 @@ $(function() {
 				employeesHTML += '<li class="out">';
 			}
 			if (employee.awesomeIndex) {
-				employeesHTML += employee.name + ' - ' + employee.occupation + ', Index: ' + employee.awesomeIndex + '</li>';
+				employeesHTML += employee.name + ' - ' + employee.occupation + ', Awesome-Index: ' + employee.awesomeIndex + '</li>';
 			} else {
 				employeesHTML += employee.name + ' - ' + employee.occupation + '</li>';
 			}
 		});
+
+		$.each(response, function(employee) {
+			var sum = 0;
+			if (employee.awesomeIndex) {
+				for (var i = 0; i < employee.length; i++) {
+					sum += JSON.parse(employee[i]).awesomeIndex;
+				}
+				avg += sum/employee.length;
+
+				return avg;
+			}
+		});
 		employeesHTML += '</ul>';
+		avgIndex += avg + '</h2>';
 		$('#employeeList').html(employeesHTML);
+		$('#awesome-index').html(avgIndex);
 	}); //end getJSON
 	
 }); //end ready
