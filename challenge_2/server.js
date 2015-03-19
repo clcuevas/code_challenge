1431c7c23d4d6b573155a39c3746f30b58608aaf
Claudia Cuevas <omordn@gmail.com>
2015-03-19 13:12:02 -0700

    adding minor error checking

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,15 @@
 var http = require('http'),
 		fs = require('fs'),
-		index = fs.readFileSync('index.html');
+		index = fs.readFileSync('index2.html');
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(index);
-}).listen(3000);
+var server = http.createServer(function(req, res) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(index);
+	}).listen(3000);
+
+server.on('error', function(e) {
+	console.log('Error: ' + e);
+});
+
+console.log("Successful connection on port 3000");
+
