b0d40a19cad225465b895b67215f57ef0ce1bf50
Claudia Cuevas <omordn@gmail.com>
2015-03-19 12:34:41 -0700

    working node server.js file

---
+++ challenge_2/server.js
@@ -0,0 +1,8 @@
+var http = require('http'),
+		fs = require('fs'),
+		index = fs.readFileSync('index.html');
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);
+}).listen(3000);
