e42abcc11455acc53fe2e6417cde78282815b2c9
Claudia Cuevas <omordn@gmail.com>
2015-03-18 20:55:58 -0700

    cleaned file a bit

--- challenge_1/script_JS.js
+++ challenge_1/script_JS.js
@@ -8,9 +8,10 @@ var employees = [
 	{ name: "Zaphod",
 		occupation: "President of the Galaxy" }
 ];
+var sum = 0;
 
 function sumAwesomeIndex() {
-	var sum = 
+
 }
 
 function avgAwesomeIndex() {
@@ -29,23 +30,4 @@ function avgAwesomeIndex() {
 }
 
 avgAwesomeIndex();
-
-// var programmers = [
-// { name: "Bob", occupation: "programmer", awesomeIndex: 7, },
-// { name: "Alice", ccupation: "programmer", awesomeIndex: 9, },
-// { name: "Zaphod", occupation: "President of the Galaxy" }
-// ];
-
-// var jsButton = document.getElementById("javascript-button");
-// var javascriptFunction = function () {
-//   var averageAwesome = 0;
-//   for (var i = 0; i < programmers.length; i++) {
-//     if (programmers[i]["awesomeIndex"]){
-//       averageAwesome += programmers[i]["awesomeIndex"];
-//     }
-//   }
-// document.getElementById("awesome-index").innerHTML = averageAwesome;
-// };
-
-// jsButton.addEventListener("click", javascriptFunction, false);
 
